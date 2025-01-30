69194ae1b3e26897df497c3c8554801ab7037327
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-30 18:11:12 -0300

    Add hover effect over the grid

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
+let squaresPerSide = 16
+
+for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+
     const square = document.createElement("div");
+    
     container.appendChild(square);
+    
+    square.style.height = (800/squaresPerSide) + "px"
+    square.style.width = (800/squaresPerSide) + "px";
+
+    square.addEventListener("mouseover", () => {
+        
+        let randomColors = []
+        for (let j = 0; j < 3; j++) {
+            randomColors[j] = Math.random() * (255 + 1);
+        }
+        square.style.backgroundColor = "rgb(" + randomColors[0] + "," + randomColors[1] + "," + randomColors[2] + ")"
+
+
+    })
+
+
+
+
+
+
 }
