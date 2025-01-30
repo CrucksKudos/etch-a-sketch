e88ded2b3e91414f676657e7c374faca87235d84
CrucksKudos <29085362+CrucksKudos@users.noreply.github.com>
2025-01-30 18:55:02 -0300

    Implement button to change number of squares per side

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
+function makeGrid(squaresPerSide) {
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+
+        const square = document.createElement("div");
+        
+        container.appendChild(square);
+        
+        square.style.height = (800/squaresPerSide) + "px"
+        square.style.width = (800/squaresPerSide) + "px";
+
+        square.addEventListener("mouseover", () => {
+            
+            let randomColors = []
+            for (let j = 0; j < 3; j++) {
+                randomColors[j] = Math.random() * (255 + 1);
+            }
+            square.style.backgroundColor = "rgb(" + randomColors[0] + "," + randomColors[1] + "," + randomColors[2] + ")"
+
+
+        })
+}
+}
+
+function removeGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+      }
+}
+
+
+
 const container = document.querySelector(".container");
 
-let squaresPerSide = 16
+let squaresPerSide = 16;
 
-for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+makeGrid(squaresPerSide)
 
-    const square = document.createElement("div");
-    
-    container.appendChild(square);
-    
-    square.style.height = (800/squaresPerSide) + "px"
-    square.style.width = (800/squaresPerSide) + "px";
+console.log(squaresPerSide)
 
-    square.addEventListener("mouseover", () => {
-        
-        let randomColors = []
-        for (let j = 0; j < 3; j++) {
-            randomColors[j] = Math.random() * (255 + 1);
-        }
-        square.style.backgroundColor = "rgb(" + randomColors[0] + "," + randomColors[1] + "," + randomColors[2] + ")"
+const button = document.createElement("button");
 
+document.body.appendChild(button)
 
-    })
+button.textContent = "Click here to define Squares per Side"
 
+button.addEventListener("click", () => {
+    do {
+        squaresPerSide = prompt("Number of Squares per Side:")
+    }
+    while (squaresPerSide < 0 || squaresPerSide > 100)
+    removeGrid();
+    makeGrid(squaresPerSide)
+})
 
-
-
-
-
-}
